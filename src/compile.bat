c855596636ba3d8ae13584b010272833a286d014
cuixiping <cuixiping@gmail.com>
2014-03-26 15:07:25 +0800

    update

--- src/compile.bat
+++ src/compile.bat
@@ -2,18 +2,21 @@
 REM *** Compile js with ClosureCompiler on windows platform.
 
 SET output=..\bin\Text-Responser\
+SET ccpath=D:\DevTools\ClosureCompiler.jar
+SET ccopts=--compilation_level=ADVANCED_OPTIMIZATIONS
 
 echo Copying images and html ......
+COPY /Y logo-16.png %output%
+COPY /Y logo-48.png %output%
 COPY /Y logo-128.png %output%
 COPY /Y manifest.json %output%
 COPY /Y options.html %output%
-COPY /Y background.html %output%
 
 echo Compiling background.js ......
-java -jar D:\DevTools\ClosureCompiler.jar --compilation_level=ADVANCED_OPTIMIZATIONS --js=background.js --js_output_file=%output%background.js
+java -jar %ccpath% %ccopts% --js=background.js --js_output_file=%output%background.js
 
 echo Compiling options.js ......
-java -jar D:\DevTools\ClosureCompiler.jar --compilation_level=ADVANCED_OPTIMIZATIONS --js=options.js --js_output_file=%output%options.js
+java -jar %ccpath% %ccopts% --js=options.js --js_output_file=%output%options.js
 
 echo Completed
 PAUSE
